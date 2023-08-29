bdbadce75da125e9f9469562cf2c998bc0295c39
GDavidDias <guillermo.david.dias@gmail.com>
2023-08-29 12:37:29 -0300

    resume cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,13 +1,27 @@
 import { useSelector } from 'react-redux';
 import style from './Cart.module.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ProductItemCart from '../ProductItemCart/ProductItemCart';
 
 const Cart = () =>{
     const cartSG = useSelector((state)=>state.cart.cart);
+    const[quantity, setQuantity] = useState(0);
+    const[amount, setAmount] = useState(0);
+
+    const recorreProductos = () =>{
+        let count=0;
+        let amount=0;
+        cartSG.forEach(product => {
+            count += product.count;
+            amount += product.count*Number(product.price);
+        });
+        setQuantity(count);
+        setAmount(amount);
+    };
 
     useEffect(()=>{
         console.log("que tiene cartSG: ",cartSG);
+        recorreProductos();
     },[cartSG])
 
     return(
@@ -28,7 +42,34 @@ const Cart = () =>{
                         }
                     </div>
                     <div className={style.right}>
-                        
+                        <div className={style.resumeTitle}>
+                            <h1>Resumen de compra</h1>
+                        </div>
+                        <div className={style.resumeProduct}>
+                            <div>
+                                <h3>{`Productos (${quantity})`}</h3>
+                            </div>
+                            <div>
+                                <h3>{`$${amount}`}</h3>
+                            </div>
+                        </div>
+                        <div className={style.resumeOtros}>
+                            <div>
+                                <h3>{`Otros Gastos`}</h3>
+                            </div>
+                            <div>
+                                {`$0`}
+                            </div>
+
+                        </div>
+                        <div className={style.resumeTotal}>
+                            <div>
+                                <h1>Total</h1> 
+                            </div>
+                            <div>
+                                {`$${amount}`}
+                            </div>
+                        </div>
                     </div>
                 </div>
 
