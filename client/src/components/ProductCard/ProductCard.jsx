be277666fef9cfbab3cb319cfd3ce1872f9f6a17
GDavidDias <guillermo.david.dias@gmail.com>
2023-09-03 22:43:58 -0300

    modal product

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -2,22 +2,17 @@ import { useDispatch } from 'react-redux';
 import style from './ProductCard.module.css';
 import { addCart } from '../../redux/cartSlice';
 import axios from 'axios';
-// import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';//?AGREGO POR MERCADOPAGO
-// import { useState } from 'react';
-// import Modal from '../Modal/Modal';
-// import { useModal } from '../../hooks/useModal';
-// const URL = 'http://localhost:3001';
+import { useState } from 'react';
+import Modal from '../Modal/Modal';
+import { useModal } from '../../hooks/useModal';
+
 
 
 const ProductCard = ({data}) =>{
-    // const[isOpenModalBuy, openModalBuy, closeModalBuy]=useModal(false);
-
-    // const[preferenceId, setPreferenceId] = useState(null); //?AGREGO POR MERCADOPAGO
-    // initMercadoPago('TEST-266d467d-bcd2-4992-a465-7b7ab11e048a'); //?AGREGO POR MERCADOPAGO
+    const[isOpenModalProduct, openModalProduct, closeModalProduct]=useModal(false);
 
     const {id,image,title,description,price} = data;
-    // console.log('que tiene prop product: ', product)
-    // console.log('que tiene prop product.title: ', product.product.title)
+
     const dispatch = useDispatch();
 
     const addProductCart = (data) =>{
@@ -25,40 +20,16 @@ const ProductCard = ({data}) =>{
         dispatch(addCart(data));
     };
 
-    // const createPreference = async (title,price) =>{
-    //     console.log('que tiene title: ', title)
-    //     console.log('que tiene price: ', price)
-        
-    //     try{
-    //         const response = await axios.post(`${URL}/create_preference`,{
-    //             title: title,
-    //             price: price,
-    //             quantity: 1,
-    //             currency_id: 'ARS',
-    //         });
-    //         const{id}=response.data;
-    //         return id;
-
-    //     }catch(error){
-    //         console.log('error en createPreference: ',error.message);
-    //     }
-    // };
-
-    // const handleBuy = async(data) =>{
-    //     const{title,price} = data;
-    //     openModalBuy();
-    //     const id = await createPreference(title,price);
-    //     if(id){
-    //         setPreferenceId(id);
-    //     }
-    // }
+    const handleImage = ()=>{
+        openModalProduct();
+    }
 
     return(
         <>
             <div className='bg-white w-48 h-64 border-2 border-slate-400 flex flex-col items-center gap-1 bg-clip-content'>
 
                 <div className={style.containerImage}>
-                    <img src={image}/>
+                    <img src={image} onClick={handleImage}/>
                 </div>
                 <h1 className='text-lg font-bold text-center truncate'>{`${title}`}</h1>
                 <div className='max-w-xxs  overflow-hidden pl-2 pr-2 w-48 h-5'>
@@ -74,18 +45,19 @@ const ProductCard = ({data}) =>{
                     >Agregar al Carrito</button>
                 </div>
             </div>
-            {/* <Modal isOpen={isOpenModalBuy} closeModal={closeModalBuy}>
-            {preferenceId && 
+            <Modal isOpen={isOpenModalProduct} closeModal={closeModalProduct}>
                 <div>
-                    <div className={style.containerImageModal}>
-                        <img src={image}/>
+                    <div>
+                        <img src={image} className='cursor-pointer'/>
                     </div>
-                    <h1 className='text-lg font-bold text-center'>{`${title} $${price}`}</h1>
+                    <h1 className='text-lg font-bold text-center'>{`${title}`}</h1>
                     <h3 className='text-left text-sm'>{description}</h3>
-                    <Wallet initialization={{preferenceId, redirectMode:'modal'}}/>
+                    <h1 className='text-lg font-bold text-center'>{`$${price}`}</h1>
+                    
+                    
                 </div>
-            }
-            </Modal> */}
+            
+            </Modal>
         </>
     )
 };
