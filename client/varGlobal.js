ce880c521ef085f95ee15be75f14b8bbfa1524b5
GDavidDias <guillermo.david.dias@gmail.com>
2023-09-04 01:29:16 -0300

    railway path

--- client/varGlobal.js
+++ client/varGlobal.js
@@ -1,4 +1,4 @@
-const URL = 'https://ecommerce-production-c72e.up.railway.app/';
+const URL = 'https://ecommerce-production-c72e.up.railway.app';
 //const URL = 'http://localhost:3001';
 
 export {URL}
