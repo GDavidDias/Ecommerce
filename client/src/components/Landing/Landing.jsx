ac04388d6973de5f0661193b22cc74a8b1c493f6
GDavidDias <guillermo.david.dias@gmail.com>
2023-09-14 15:59:31 -0300

    style tailwindcss postcss

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
     return (
-            <div className="h-screen flex flex-col justify-center bg-green">
+            <div className="h-screen flex flex-col justify-center bg-gradient-to-b from-[#85A0F2] to-[#4BF286]">
                 <div>
                     <h1 className="text-2xl font-bold text-center"
                     >Bienvenidos a la Tienda Virtual</h1>
@@ -10,7 +10,7 @@ const Landing = () => {
                 <div className="flex justify-center mt-2">
                     <Link to={'/home'}>
                         <button 
-                            className="bg-orange hover:bg-blue text-white font-bold w-40 h-8"
+                            className="bg-orange hover:bg-green text-white font-bold w-40 h-8"
                         >Ingresar</button>
                     </Link>
                 </div>
--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -26,10 +26,11 @@ const ProductCard = ({data}) =>{
 
     return(
         <>
-            <div className='bg-white w-48 h-64 border-2 border-slate-400 flex flex-col items-center gap-1 bg-clip-content'>
+            <div className='bg-white w-48 h-64 border-2 border-slate-200 flex flex-col items-center gap-1 bg-clip-content'>
 
-                <div className={style.containerImage}>
-                    <img src={image} onClick={handleImage}/>
+                {/* <div className={style.containerImage}> */}
+                <div className='w-48 h-32 flex items-center justify-center position-relative'>
+                    <img src={image} onClick={handleImage} className='w-full h-full position-absolute'/>
                 </div>
                 <h1 className='text-lg font-bold text-center truncate'>{`${title}`}</h1>
                 <div className='max-w-xxs  overflow-hidden pl-2 pr-2 w-48 h-5'>
